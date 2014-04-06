fb15a64f58da465e25b4bf7a372d10d695b0b209
Duy Trần <duytran.bkict@gmail.com>
2014-04-07 01:37:11 +0700

    Init commit

---
+++ index.php
@@ -0,0 +1,361 @@
+<?php ?>
+
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Boutique</title>
+
+		<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+		<link rel="stylesheet" href="css/bootstrap-glyphicon.min.css" type="text/css" />
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+		<link rel='stylesheet' href='css/font-lobster.css' type='text/css'>
+		<link rel='stylesheet' href='css/font-myriadpro.css' type='text/css'>
+		<link rel='stylesheet' href='css/font-kreon.css' type='text/css'>
+		<link rel='stylesheet' href='css/font-cookie.css' type='text/css'>
+
+		<script src="js/jquery.min.js" type="text/javascript"></script>
+		<script src="js/bootstrap.min.js" type="text/javascript"></script>
+		<script type="text/javascript">
+			$.noConflict();
+		</script>
+	</head>
+
+	<body>
+		<div id="color-stripes"></div>
+
+		<div id="header-wrapper">
+			<div class="container">
+				<div id="header-logo">
+					<h1>Boutique</h1>
+					<span>Phụ kiện, Quà tặng, Trang trí</span>
+				</div>
+				<div id="header-menu">
+					<ul id="secondary-menu" class="nav">
+
+						<li><a href="#">Mô hình</a></li>
+						<li><a href="#">Nội thất</a></li>
+						<li><a href="#">Văn phòng phẩm</a></li>
+						<li><a href="#">Phụ kiện</a></li>
+						<li><a href="#">Thủy tinh & Gốm sứ</a></li>
+						<li><a href="#">Manga & Anime</a></li>
+						<li><a href="#">Sản phẩm cá nhân</a></li>
+						<li><a href="#">Xem tất cả</a></li>
+
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="clearfix" ></div>
+
+		<div id="slide-wrapper">
+			<div class="top-bg">
+				<div class="bottom-bg">
+					<div class="container">
+
+
+						<div id="slides" class="carousel slide fade" data-ride="carousel">
+							<!-- Indicators -->
+							<ol class="carousel-indicators">
+								<li data-target="#slides" data-slide-to="0" class="active"></li>
+								<li data-target="#slides" data-slide-to="1"></li>
+								<li data-target="#slides" data-slide-to="2"></li>
+							</ol>
+
+							<!-- Wrapper for slides -->
+							<div class="carousel-inner">
+								<div class="item active">
+									<div class="featured-image">
+										<a href="#">
+											<img width="380" height="230" src="img/products/rubik.png" alt="Rubic Standard" />
+										</a>
+										<div class="featured-price">
+											<a class="add_cart et-shop-item" href="#">Thêm Giỏ Hàng</a>
+											<a class="more_info" href="#">Xem Thêm</a>
+											<span class="price">75 k</span>
+											<span class="currency_sign"></span>
+										</div>
+									</div>
+									<div class="featured-description">
+										<h2 class="featured-title">
+											<a href="#">Rubic Standard</a>
+										</h2>
+										<p>Rubik 3x3x3 chính hãng là một trong số các loại rubik cơ bản không thể thiếu trong bộ sưu tập của Rubiker</p>
+										<a class="readmore" href="#">
+											<span>Xem Thêm</span>
+										</a>
+									</div>
+								</div>
+								<div class="item">
+									<div class="featured-image">
+										<a href="#">
+											<img width="380" height="230" src="img/products/buzz.png" alt="Đồ chơi Buzz Lightyear"></img>
+										</a>
+										<div class="featured-price">
+											<a class="add_cart et-shop-item" href="#">Thêm Giỏ Hàng</a>
+											<a class="more_info" href="#">Xem thêm</a>
+											<span class="price">680 k</span>
+											<span class="currency_sign"></span>
+										</div>
+									</div>
+									<div class="featured-description">
+										<h2 class="featured-title">
+											<a href="#">Đồ chơi Buzz Lightyear</a>
+										</h2>
+										<p>Với sản phẩm đồ chơi Buzz Lightyear, chắc hẳn sẽ làm thỏa mãn với những ai biết đến loạt phim hoạt hình nổi tiếng Toy Story....</p>
+										<a class="readmore" href="#">
+											<span>Xem thêm</span>
+										</a>
+									</div>
+								</div>
+								<div class="item">
+									<div class="featured-image">
+										<a href="#">
+											<img width="380" height="230" src="img/products/pillows.png" alt="Gối ôm"></img>
+										</a>
+										<div class="featured-price">
+											<a class="add_cart et-shop-item" href="#">Add To Cart</a>
+											<a class="more_info" href="#">Xem thêm</a>
+											<span class="price">300 k</span>
+											<span class="currency_sign"></span>
+										</div>
+									</div>
+									<div class="featured-description">
+										<h2 class="featured-title">
+											<a href="#">Gối ôm</a>
+										</h2>
+										<p>Ut lacinia dolor sed diam auctor sodales. Morbi dapibus suscipit laoreet. Quisque bibendum iaculis augue tempus posuere. Donec nisi odio, volutpat vel vestibulum nec, dapibus a nisl....</p>
+										<a class="readmore" href="#">
+											<span>Xem thêm</span>
+										</a>
+									</div>
+								</div>
+							</div>
+
+							<!-- Controls -->
+							<a id="left-arrow" href="#slides" data-slide="prev"></a>
+							<a id="right-arrow" href="#slides" data-slide="next"></a>
+						</div>
+
+
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="clearfix"></div>
+
+		<div class="container" >
+			<div id="images" class="row">
+				<div class="col-xs-3">
+					<a href="http://www.google.com" target="blank"><img src="img/voucher.jpg" class="img-rounded"/></a>
+				</div>					
+				<div class="col-xs-3">
+					<a href="http://www.google.com" target="blank"><img src="img/huongdanmuahang.jpg" class="img-rounded"/></a>
+				</div>
+				<div class="col-xs-3">
+					<a href="http://www.google.com" target="blank"><img src="img/modaily.jpg" class="img-rounded"/></a>
+				</div>
+				<div class="col-xs-3">
+					<a href="http://www.facebook.com" target="blank"><img src="img/fanpage.jpg" class="img-rounded"/></a>
+				</div>		
+			</div>
+		</div>	
+
+		<div class="container">
+
+			<div id="content">
+				<div id="main-bg">
+					<div id="main-bottom-bg">
+						<div id="left-area">
+							<h3 class="offer-title">
+								<span>Sản phẩm mới</span>
+							</h3>						
+							<div style="height:63px;"></div>
+
+							<div class="row row-product">
+								<div class="cell-product col-xs-3">
+									<div class="main-item">
+										<a href="#">
+											<img class="thumb" src="img/products/1.jpg"></img>
+											<span class="price-tag">15</span>
+										</a>
+										<h3><a href="#">School Supplies</a></h3>
+									</div>
+								</div>
+								<div class="cell-product col-xs-3">
+									<div class="main-item">
+										<a href="#">
+											<img class="thumb" src="img/products/2.jpg"></img>
+											<span class="price-tag">15</span>
+										</a>
+										<h3><a href="#">School Supplies</a></h3>
+									</div>
+								</div>
+								<div class="cell-product col-xs-3">
+									<div class="main-item">
+										<a href="#">
+											<img class="thumb" src="img/products/3.jpg"></img>
+											<span class="price-tag">15</span>
+										</a>
+										<h3><a href="#">School Supplies</a></h3>
+									</div>
+								</div>
+								<div class="cell-product col-xs-3">
+									<div class="main-item">
+										<a href="#">
+											<img class="thumb" src="img/products/4.jpg"></img>
+											<span class="price-tag">15</span>
+										</a>
+										<h3><a href="#">School Supplies</a></h3>
+									</div>
+								</div>
+							</div>
+
+							<div class="row row-product">
+								<div class="cell-product col-xs-3">
+									<div class="main-item">
+										<a href="#">
+											<img class="thumb" src="img/products/5.jpg"></img>
+											<span class="price-tag">15</span>
+										</a>
+										<h3><a href="#">School Supplies</a></h3>
+									</div>
+								</div>
+								<div class="cell-product col-xs-3">
+									<div class="main-item">
+										<a href="#">
+											<img class="thumb" src="img/products/6.jpg"></img>
+											<span class="price-tag">15</span>
+										</a>
+										<h3><a href="#">School Supplies</a></h3>
+									</div>
+								</div>
+								<div class="cell-product col-xs-3">
+									<div class="main-item">
+										<a href="#">
+											<img class="thumb" src="img/products/7.jpg"></img>
+											<span class="price-tag">15</span>
+										</a>
+										<h3><a href="#">School Supplies</a></h3>
+									</div>
+								</div>
+								<div class="cell-product col-xs-3">
+									<div class="main-item">
+										<a href="#">
+											<img class="thumb" src="img/products/8.jpg"></img>
+											<span class="price-tag">15</span>
+										</a>
+										<h3><a href="#">School Supplies</a></h3>
+									</div>
+								</div>
+							</div>
+
+							<div class="row row-product">
+								<div class="cell-product col-xs-3">
+									<div class="main-item">
+										<a href="#">
+											<img class="thumb" src="img/products/9.jpg"></img>
+											<span class="price-tag">15</span>
+										</a>
+										<h3><a href="#">School Supplies</a></h3>
+									</div>
+								</div>
+								<div class="cell-product col-xs-3">
+									<div class="main-item">
+										<a href="#">
+											<img class="thumb" src="img/products/10.jpg"></img>
+											<span class="price-tag">15</span>
+										</a>
+										<h3><a href="#">School Supplies</a></h3>
+									</div>
+								</div>
+								<div class="cell-product col-xs-3">
+									<div class="main-item">
+										<a href="#">
+											<img class="thumb" src="img/products/11.jpg"></img>
+											<span class="price-tag">500k</span>
+										</a>
+										<h3><a href="#">School Supplies</a></h3>
+									</div>
+								</div>
+							</div>
+
+						</div>
+
+						<div id="right-area" class="right-area-bg">
+
+							<h2>Danh mục sản phẩm</h2>									
+							<ul class="right-area-bg" style="padding-top: 20px">
+								<li>
+									<a href="#">Mô hình</a>
+								</li>
+								<li>
+									<a href="#">Nội thất</a>
+								</li>
+								<li>
+									<a href="#">Văn phòng phẩm</a>
+								</li>
+								<li>
+									<a href="#">Phụ kiện</a>
+								</li>
+								<li>
+									<a href="#">Thủy tinh & Gốm sứ</a>
+								</li>
+								<li>
+									<a href="#">Manga & Anime</a>
+								</li>
+								<li>
+									<a href="#">Sản phẩm cá nhân</a>
+								</li>
+							</ul>
+
+
+						</div>
+
+						<div class="clearfix"></div>
+
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+		<div id="footer">
+			<div class="top-bg">
+				<div class="bottom-bg" style="height: 280px">
+					<div class="container">
+						<div class="col-xs-6">
+							<h3>About us</h3>
+							<p>Sed arcu lectus, consectetur at auctor et, dictum eget urna. In porttitor felis at lectus feugiat non suscipit est pretium. Sed fermentum ultrices mauris eget porta. Sed id magna erat. </p>
+						</div>
+						<div class="col-xs-3">
+							<h3>Contacts</h3>
+							<ul>
+								<li><a href="#">Google</a></li>
+								<li><a href="#">Facebook</a></li>
+							</ul>
+						</div>
+						<div class="col-xs-3">
+							<h3>Our partners</h3>
+							<ul>
+								<li><a href="#">Google</a></li>
+								<li><a href="#">Facebook</a></li>
+								<li><a href="#">Google</a></li>
+								<li><a href="#">Facebook</a></li>
+								<li><a href="#">Facebook</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="footer-bottom">
+			<center>Copyright &copy; 2014</center>
+		</div>
+
+		<div id="color-stripes"></div>
+	</body>
+</html>
